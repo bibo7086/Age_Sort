26069731ea3784caceea3c7e1e135a7ef29e2948
Saeed Aliyu <saeedaliyu@Saeeds-MacBook-Pro.local>
2015-09-10 07:23:43 +0100

    Initial Commit

---
+++ Age_sort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Age_sort
+//
+//  Created by Saeed Aliyu on 9/10/15.
+//  Copyright (c) 2015 Saeed Aliyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
